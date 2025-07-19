f70d54442a7837df687bb88c8f0d6e440dc4bf28
Saurabh Pandey <pandeyjii917sp@gmail.com>
2025-07-19 17:50:31 +0530

    done

--- frontend/e-commerce/src/pages/signin.jsx
+++ frontend/e-commerce/src/pages/signin.jsx
@@ -13,7 +13,7 @@ const Signin = () => {
     setError("");
     setSuccess("");
     try {
-      const res = await fetch("https://e-commerce-ql1j.onrender.com/signin ", {
+      const res = await fetch("https://e-commerce-4bm4.onrender.com/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password })
